3d6345fdd3eb6c0707f8ceba4555c63de168017f
VitorDSantos <vv199910191@gmail.com>
2021-11-19 11:49:13 -0300

    adicionando a class usuario

---
+++ src/main/java/ProjetoFinal/modelo/Usuario.java
@@ -0,0 +1,25 @@
+package ProjetoFinal.modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Usuario {
+	private Long id;
+	private String nome;
+	private Integer NumeroOAB;
+	private String login;
+	private String senha;
+
+
+	
+	
+}
