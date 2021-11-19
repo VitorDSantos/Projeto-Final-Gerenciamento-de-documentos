cfd263bf8a1ff1e4a9c1796b3f21ce1f41829d27
VitorDSantos <vv19991019@gmail.com>
2021-11-19 09:37:09 -0300

    Adicionando  branches e os pacotes

--- src/main/java/ProjetoFinal/Application/ApiGerenciadorDocApplication.java
+++ src/main/java/ProjetoFinal/Application/ApiGerenciadorDocApplication.java
@@ -1,4 +1,4 @@
-package ProjetoFinal.ApiGerenciadorDoc;
+package ProjetoFinal.Application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
