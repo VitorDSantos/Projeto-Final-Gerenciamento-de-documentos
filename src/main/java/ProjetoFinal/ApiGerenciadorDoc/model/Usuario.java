b4796e8f0657298b969b61801e45c624d1cdd260
VitorDSantos <vv199910191@gmail.com>
2021-11-22 09:41:24 -0300

    ajustando a classe usuario

--- src/main/java/ProjetoFinal/ApiGerenciadorDoc/model/Usuario.java
+++ src/main/java/ProjetoFinal/ApiGerenciadorDoc/model/Usuario.java
@@ -6,11 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@EqualsAndHashCode
-@NoArgsConstructor
-@AllArgsConstructor
+
 
 public class Usuario {
 	private Long id;
@@ -18,8 +14,46 @@ public class Usuario {
 	private Integer NumeroOAB;
 	private String login;
 	private String senha;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getNumeroOAB() {
+		return NumeroOAB;
+	}
+	public void setNumeroOAB(Integer numeroOAB) {
+		NumeroOAB = numeroOAB;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public Usuario(Long id, String nome, Integer numeroOAB, String login, String senha) {
+		this.id = id;
+		this.nome = nome;
+		NumeroOAB = numeroOAB;
+		this.login = login;
+		this.senha = senha;
+	}
 
-
+	public Usuario() {
+	}
 	
 	
 }
