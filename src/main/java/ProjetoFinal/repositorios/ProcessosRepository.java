40d8edb969ce9e88d45483cd8555073261524a00
Katherin} <katherinpereda19@hotmail.com>
2021-11-21 23:28:45 -0300

    Adicionando a config do application.properties

--- src/main/java/ProjetoFinal/repositorios/ProcessosRepository.java
+++ src/main/java/ProjetoFinal/repositorios/ProcessosRepository.java
@@ -1,5 +1,14 @@
 package ProjetoFinal.repositorios;
 
-public interface ProcessosRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ProjetoFinal.modelo.Processos;
+
+public interface ProcessosRepository extends JpaRepository <Processos,Integer>{
+	
+	List<Processos>findByNroProcessos(int nro_processo);
+	
+	Processos findById(int id);
 }
