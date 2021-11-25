26c437b98783eeae1b7f0ded6c4aec47b6467c0c
Rafael <rafa101993@gmail.com>
2021-11-25 13:12:27 -0300

    Criando os mapeamentos no DocumentoController

--- src/main/java/ProjetoFinal/controller/DocumentoController.java
+++ src/main/java/ProjetoFinal/controller/DocumentoController.java
@@ -1,5 +1,43 @@
 package ProjetoFinal.controller;
 
-public class DocumentoController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
+@RestController
+@RequestMapping("/api/doc")
+public class DocumentoController {
+	
+	@PostMapping
+	public ResponseEntity<String> adicionar() {
+		return new ResponseEntity<>("Documento adicionado.", HttpStatus.CREATED); 
+	}
+	
+	@GetMapping
+	public ResponseEntity<String> listar() {
+		return new ResponseEntity<>("Todos os documentos listado.", HttpStatus.OK); 
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<String> detalhar() {
+		return new ResponseEntity<>("Documento {X} listado.", HttpStatus.OK);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<String> atualizar() {
+		return new ResponseEntity<>("Documento atualizado.", HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> remover() {
+		return new ResponseEntity<>("Documento excluido", HttpStatus.OK);
+	}
+	
 }
