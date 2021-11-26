bd09e5c1d7badb37627939491f72eee60f154747
Rafael <rafa101993@gmail.com>
2021-11-26 12:23:03 -0300

    Atualizando arquivo data.sql (inserir um usuario no bd)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,1 @@
-INSERT INTO USUARIO(nome, numeroOAB, login, nroProcesso, senha) VALUES('adm', 324156, 'adm@gmail.com', 3, '12345');
-INSERT INTO USUARIO(nome, numeroOAB, login, nroProcesso, senha) VALUES('adm2', 645213, 'adm2@gmail.com', 3, '54321');
-
-INSERT INTO DOCUMENTO(
-nome, tipoDocumento, pathArquivo,
- dataUpload, usuarioProprietario,
- processos) 
- VALUES(' copia de identidade', 'pdf', 'desktop', 
- '24-11-2021', 'adm','principal');
+INSERT INTO projetofinalcmpso.usuario(login, nome, numerooab, senha) VALUES ("rafael", "Rafael Gularte Silva", 500999, "123456");
