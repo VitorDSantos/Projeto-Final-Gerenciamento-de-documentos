9cb58cc0efd274fce9a1d6227a24309fe3d14456
Rafael <rafa101993@gmail.com>
2021-11-26 12:20:08 -0300

    Adicionando opção na anotação para desabilitar a segurança para os testes.

--- src/main/java/projetofinal/ApiGerenciadorDocApplication.java
+++ src/main/java/projetofinal/ApiGerenciadorDocApplication.java
@@ -2,8 +2,9 @@ package projetofinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ApiGerenciadorDocApplication {
 
 	public static void main(String[] args) {
