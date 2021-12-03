6019905281f60bf5bcdb73e86e4033b193facfa4
Rafael <rafa101993@gmail.com>
2021-12-03 12:47:06 -0300

    Atualizando DocumentoService.java

--- src/main/java/projetofinal/service/DocumentoService.java
+++ src/main/java/projetofinal/service/DocumentoService.java
@@ -1,7 +1,14 @@
 package projetofinal.service;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import projetofinal.dto.DocumentoDto;
+import projetofinal.dto.UsuarioDto;
 import projetofinal.form.FormDocumento;
 import projetofinal.model.Documento;
 import projetofinal.repository.DocumentoRepository;
@@ -11,18 +18,52 @@ public class DocumentoService {
 
 	@Autowired
 	private DocumentoRepository documentoRepository;
-	
+
 	public void cadastrar(FormDocumento documentoForm) {
-		
-		Documento documento = new Documento();
-		
-		documento.setNome(documentoForm.getNome());
-		documento.setDataUpload(documentoForm.getDataUpload());
-		documento.setPathArquivo(documentoForm.getPathArquivo());
-		documento.setTipoDocumento(documentoForm.getTipoDocumento());
-		documento.setUsuarioProprietario(documentoForm.getUsuarioProprietario());
-		
-		documentoRepository.save(documento);
+
+		Documento cadastrarDocumento = new Documento();
+
+		cadastrarDocumento.setNome(documentoForm.getNome());
+		cadastrarDocumento.setDataUpload(documentoForm.getDataUpload());
+		cadastrarDocumento.setPathArquivo(documentoForm.getPathArquivo());
+		cadastrarDocumento.setTipoDocumento(documentoForm.getTipoDocumento());
+		cadastrarDocumento.setUsuarioProprietario(documentoForm.getUsuarioProprietario());
+
+		documentoRepository.save(cadastrarDocumento);
 	}
-	
+
+	@Transactional
+	public void atualizar(Long id, FormDocumento documentoForm) {
+
+		Documento atualizarDocumento = documentoRepository.getById(id);
+
+		atualizarDocumento.setNome(documentoForm.getNome());
+		atualizarDocumento.setUsuarioProprietario(documentoForm.getUsuarioProprietario());
+
+	}
+
+	public List<DocumentoDto> listar() {
+		List<Documento> documentos;
+		documentos = documentoRepository.findAll();
+		return DocumentoDto.converter(documentos);
+	}
+
+	public DocumentoDto detalhar(Long id) {
+		Documento documento = documentoRepository.getById(id);
+		DocumentoDto documentoDto = new DocumentoDto();
+		
+		documentoDto.setId(documento.getId());
+		documentoDto.setNome(documento.getNome());
+		documentoDto.setPathArquivo(documento.getPathArquivo());
+		documentoDto.setDataUpload(documento.getDataUpload());
+		documentoDto.setTipoDocumento(documento.getTipoDocumento());
+		documentoDto.setUsuarioProprietario(documento.getUsuarioProprietario());
+		
+		return documentoDto;
+		
+	}
+
+	public void remover(Long id) {
+		documentoRepository.deleteById(id);
+	}
 }
